51201fb983a7a0606906434a353a9655b15dbabc
jed <jed@casecommons.org>
2014-01-12 22:46:22 -0500

    Initial commit - added base files

---
+++ date_court_id_grabber.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+from pandas import *
+
+def extract_evictions_table(html):
+
+  soup = BeautifulSoup(html)
+
+  table = soup.findAll('table')[17]
+
+  rows = soup.findAll('table')[17].findAll('td')
+
+  date_filed = []
+  case_number = []
+  style_of_case = []
+  case_type = []
+  location = []
+
+
+  i=0
+  for row in rows:
+    i=i+1
+
+    if i > 8:
+      if (i - 8) % 6 == 1:
+        line = row.contents[0].encode('utf-8').strip("\t\r\n")
+        date_filed.append(line)
+      if (i - 8) % 6 == 2:
+        link=row.find('a')
+        line = link.contents[0].encode('utf-8').strip("\t\r\n")
+        case_number.append(line)
+      if (i - 8) % 6 == 3:
+        line = row.contents[0].encode('utf-8').strip("\t\r\n")
+        style_of_case.append(line)
+      if (i - 8) % 6 == 4:
+        line = row.contents[0].encode('utf-8').strip("\t\r\n")
+        case_type.append(line)
+      if (i - 8) % 6 == 5:
+        line = row.contents[0].encode('utf-8').strip("\t\r\n")
+        location.append(line)
+
+  case_info=DataFrame({'date':date_filed,'case_number':case_number,'style_of_case':style_of_case,'case_type':case_type,'location':location})
+  # evictions = case_info[case_info.case_type.str.strip() == "AC Rent and Possession "]
+  return case_info
---
+++ initial_test_loop.py
@@ -0,0 +1,77 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
+from pandas import *
+
+import unittest, time, re, csv
+
+execfile("date_court_id_grabber.py")
+
+filename = "test_eviction_list.csv"
+
+class InitialTestLoop(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.courts.mo.gov/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_initial_test_loop(self):
+        with open('small_week_test.csv', 'rb') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                week = row
+                driver = self.driver
+                driver.get(self.base_url + "/casenet/base/welcome.do")
+                driver.find_element_by_id("dateSearchImg").click()
+                Select(driver.find_element_by_id("courtId")).select_by_visible_text("6th Judicial Circuit (Platte County)")
+                driver.find_element_by_id("inputVO.startDate").clear()
+                driver.find_element_by_id("inputVO.startDate").send_keys(week)
+                driver.find_element_by_id("findButton").click()
+                
+                #automates loop through of pages within a week
+                totalRecords = driver.find_element_by_id("totalRecords").get_attribute("value")
+                currentRecord = driver.find_element_by_id("startingRecord").get_attribute("value")
+                page = 2
+                while(int(currentRecord) < int(totalRecords)):
+                    html = driver.page_source
+                    evics = extract_evictions_table(html)
+                    print evics
+                    evics.to_csv(filename, header = False, mode = 'a')
+                    page_change_script = "goToThisPage( '" + str(page) + "' )"
+                    driver.execute_script(page_change_script)
+                    currentRecord = driver.find_element_by_id("startingRecord").get_attribute("value")
+                    page = page + 1
+
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException, e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
